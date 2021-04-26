2809fab795930b44b17dd4bb052c0f53981e5e7a
Joao Vitor Gatti Teixeira <joaovgatti@pop-os.localdomain>
2021-04-26 12:06:17 -0300

    refactor

--- Lab4/main.c
+++ Lab4/main.c
@@ -10,6 +10,7 @@ pthread_cond_t cond;
 
 void *thread1(void *arg){
     pthread_mutex_lock(&mutex);
+    //espera todas as threads executarem
     while(x < 3){
         pthread_cond_wait(&cond, &mutex);
     }
@@ -21,11 +22,13 @@ void *thread1(void *arg){
 
 void *thread2(void *arg){
     pthread_mutex_lock(&mutex);
+    //espera pela thread4
     if(x < 1){
         pthread_cond_wait(&cond, &mutex);
     }
     x++;
     printf("Fique a vontade.\n");
+    //sinaliza para thread1
     pthread_cond_signal(&cond);
     pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
@@ -33,11 +36,13 @@ void *thread2(void *arg){
 
 void *thread3(void *arg){
     pthread_mutex_lock(&mutex);
+    //espera pela thread4
     if(x < 1){
         pthread_cond_wait(&cond, &mutex);
     }
     x++;
     printf("Sente-se por favor.\n");
+    //sinaliza para thrad1
     pthread_cond_signal(&cond);
     pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
@@ -47,6 +52,7 @@ void *thread4(void *arg){
     printf("Seja bem vindo.\n");
     pthread_mutex_lock(&mutex);
     x++;
+    //sinaliza para o restante das threads
     pthread_cond_broadcast(&cond);
     pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
@@ -56,6 +62,10 @@ int main() {
 
     pthread_t *tid = (pthread_t*)malloc(sizeof(pthread_t)*4);
 
+    /*
+     *  Array de funcoes.
+     *  "Two or more, use a for"
+     * */
     typedef void (f)(void);
     f* func[4] = {(f *) &thread1, (f *) &thread2, (f *) &thread3, (f *) &thread4};
 
